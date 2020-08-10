bc84bf15d7f9232bde2775636b83978f58d9b23b
yolandalopezfernandez <yolandaa020@outlook.com>
2020-08-10 19:25:57 -0300

    primer punto

--- uno.js
+++ uno.js
@@ -9,5 +9,61 @@ pedir datos por prompt y mostrar por document.write o console.log
 */
 function mostrar()
 {
-	alert("uno");
+	let nombre;
+	let edad;
+	let sexo;
+	let peso;
+	let hombreMasPesado;
+	let hombrePeso;
+
+    let contadorFemenino = 0;
+    let acumuladorEdad = 0;
+    let flagHombre = 0;
+    
+
+
+    for( i = 0; i < 5 ; i++){
+
+        nombre = prompt("Ingrese nombre: ");
+        
+
+		peso = parseInt(prompt("Ingrese peso: "));
+		while(!(peso > 0)){
+			peso = parseInt(prompt("Error. Ingrese peso mayor a cero: "));
+		}
+
+        edad = parseInt(prompt("Ingrese edad: "));
+		acumuladorEdad+= edad;
+		while(!(edad > 0)){
+			edad = parseInt(prompt("Error. Ingrese edad mayor a cero: "));
+		}
+
+		sexo = prompt("Ingrese sexo: ");
+		while(!(sexo == 'f' || sexo == 'm')){
+			sexo = prompt("Ingrese sexo: 'f' o 'm' ");
+		}
+
+        if(sexo == 'f'){
+            contadorFemenino++;
+        }
+        
+
+        if(flagHombre == 0 && sexo == 'm'){
+            flagHombre = 1;
+            hombreMasPesado = nombre;
+            hombrePeso = peso;
+
+        }
+        else{
+            if(peso > hombrePeso && sexo == 'm'){
+				hombreMasPesado = nombre;
+				hombrePeso = peso;
+            }
+        }
+
+    }
+
+    console.log(`Cantidad de feminos es ${contadorFemenino},
+	\nla edad promedio en total es: ${acumuladorEdad / 5 },
+	\ny el hombre más pesado es  ${hombreMasPesado}`);
 }
