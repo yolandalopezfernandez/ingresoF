09c02ef93c00bb13e192f391c8468c6da7582c15
Christian Baus <christianbaus@hotmail.com>
2020-08-10 18:25:51 -0300

    Add files via upload

---
+++ uno.js
@@ -0,0 +1,13 @@
+/*
+1)De 5  personas que ingresan al hospital se deben tomar y 
+validar los siguientes datos.
+nombre ,peso, sexo y edad.
+a)informar la cantidad de mujeres.
+b)la edad promedio en total.
+c)el hombre mas pesado.
+pedir datos por prompt y mostrar por document.write o console.log
+*/
+function mostrar()
+{
+	alert("uno");
+}
